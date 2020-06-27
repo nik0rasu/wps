d0f963228c3bc66fec38f0529dd266a3df37d22b
int21h000 <65222613+int21h000@users.noreply.github.com>
2020-06-27 21:27:38 +0300

    Update wps.c

--- wps.c
+++ wps.c
@@ -1,12 +1,5 @@
 #include <windows.h>
-#include <string.h>
 
-void main(int argc, char **argv){
-	if(!strcmp(argv[1], "-f")){
-		char str[60] = "cmd.exe /c netsh wlan export profile folder=";
-		strcat(str, strcat(argv[2], " key=clear"));
-		WinExec(str, 0);  
-	} else {
-		WinExec("cmd.exe /c netsh wlan export profile key=clear", 0);
-	}    
-}
+void main(){
+    WinExec("cmd.exe /c netsh wlan export profile folder=Pictures, 0);    
+}"
